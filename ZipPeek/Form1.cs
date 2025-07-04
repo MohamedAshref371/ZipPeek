2109d2e09c9101e213d9a5b6565b387f97d7523d
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-07-04 16:20:37 +0300

    update

--- ZipPeek/Form1.cs
+++ ZipPeek/Form1.cs
@@ -20,18 +20,22 @@ namespace ZipPeek
 
         private async void OnlineLoadBtn_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(urlTextBox.Text))
+                return;
+
             treeZip.Nodes.Clear();
             List<ZipEntry> entries;
+            onlineLoadBtn.Enabled = false; downloadBtn.Enabled = false;
             try
             {
                 entries = await ZipReader.ReadZipEntriesAsync(urlTextBox.Text);
-            
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Error reading ZIP file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+            onlineLoadBtn.Enabled = true; downloadBtn.Enabled = true;
 
             foreach (var entry in entries)
                 TreeViewHelper.AddToTree(entry);
@@ -39,17 +43,18 @@ namespace ZipPeek
 
         private async void DownloadBtn_Click(object sender, EventArgs e)
         {
+            onlineLoadBtn.Enabled = false; downloadBtn.Enabled = false;
             try
             {
                 if (treeZip.SelectedNode?.Tag is ZipEntry entry)
                     await RemoteZipExtractor.ExtractRemoteEntryAsync(urlTextBox.Text, entry, "down");
-            
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Error extracting entry from remote ZIP file: {ex.Message}", "Extraction Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+            onlineLoadBtn.Enabled = true; downloadBtn.Enabled = true;
         }
 
         private void TreeZip_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
