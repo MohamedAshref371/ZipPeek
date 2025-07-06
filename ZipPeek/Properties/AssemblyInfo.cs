9aed01d0b6363fb416d0f1fc42d7f6ac4d7e4fc8
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-07-06 19:26:48 +0300

    v1.5

--- ZipPeek/Properties/AssemblyInfo.cs
+++ ZipPeek/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZipPeek v1.4")]
+[assembly: AssemblyTitle("ZipPeek v1.5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("github.com/MohamedAshref371")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
